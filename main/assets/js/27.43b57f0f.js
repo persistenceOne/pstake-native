87670ecd8bceed8371ec1dcf4d55fef03fb66f54
puneet2019 <59960662+puneet2019@users.noreply.github.com>
2021-12-06 07:37:43 +0000

    Deploying to gh-pages from  @ 4eaced72675ba9025c1fcc9b2f0b10ab158aedec 🚀

---
+++ main/assets/js/27.43b57f0f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{561:function(n,e,t){"use strict";t.r(e);var i={mounted:function(){window.location.assign("".concat(this.$site.base,"hub-overview/overview.html"))}},o=t(1),s=Object(o.a)(i,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=s.exports}}]);
