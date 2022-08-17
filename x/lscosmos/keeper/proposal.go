acdbfc2f48a53d9308e7a2722fab58506b36b53d
Puneet <59960662+puneet2019@users.noreply.github.com>
2022-08-17 11:14:30 +0400

    Puneet2019/keeper symentics (#206)
    * register ICA accounts.
    
    * fix keepers to import only exported keeper.
    
    * fix imports
    
    * fix imports lint

--- x/lscosmos/keeper/proposal.go
+++ x/lscosmos/keeper/proposal.go
@@ -1,11 +1,15 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	ibctransfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+	ibcchanneltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	ibcporttypes "github.com/cosmos/ibc-go/v3/modules/core/05-port/types"
-	
+	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
+
 	"github.com/persistenceOne/pstake-native/x/lscosmos/types"
 )
 
@@ -23,6 +27,23 @@ func HandleRegisterCosmosChainProposal(ctx sdk.Context, k Keeper, content types.
 
 	if content.TokenTransferPort != ibctransfertypes.PortID {
 		return sdkerrors.Wrap(ibcporttypes.ErrInvalidPort, "Only acceptable TokenTransferPort is \"transfer\"")
+	}
+
+	// checks for valid and active channel
+	channel, found := k.channelKeeper.GetChannel(ctx, content.TokenTransferPort, content.TokenTransferChannel)
+	if !found {
+		return sdkerrors.Wrap(ibcchanneltypes.ErrChannelNotFound, fmt.Sprintf("channel for ibc transfer: %s not found", content.TokenTransferChannel))
+	}
+	if channel.State != ibcchanneltypes.OPEN {
+		return sdkerrors.Wrapf(
+			ibcchanneltypes.ErrInvalidChannelState,
+			"channel state is not OPEN (got %s)", channel.State.String(),
+		)
+	}
+	// TODO Understand capabilities and see if it has to be/ should be claimed in lsscopedkeeper. If it even matters.
+	_, err = k.lscosmosScopedKeeper.NewCapability(ctx, host.ChannelCapabilityPath(content.TokenTransferPort, content.TokenTransferChannel))
+	if err != nil {
+		return sdkerrors.Wrapf(err, "Failed to create and claim capability for ibc transfer port and channel")
 	}
 
 	// This checks for channel being active
