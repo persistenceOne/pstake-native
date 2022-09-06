6995450c9029b43ba55613d8b5eb9fb04d56a0ba
arhamchordia <chordia.1@iitj.ac.in>
2022-09-06 18:38:20 +0400

    Added more tests in lscosmos/keeper.

---
+++ x/lscosmos/keeper/cosmos_test.go
@@ -0,0 +1,31 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *IntegrationTestSuite) TestHostChainParams() {
+	app, ctx := suite.app, suite.ctx
+
+	depositFee, err := sdk.NewDecFromStr("0.01")
+	suite.NoError(err)
+
+	restakeFee, err := sdk.NewDecFromStr("0.02")
+	suite.NoError(err)
+
+	unstakeFee, err := sdk.NewDecFromStr("0.03")
+	suite.NoError(err)
+
+	params := app.LSCosmosKeeper.GetHostChainParams(ctx)
+	suite.Equal(ChainID, params.ChainID)
+	suite.Equal(ConnectionID, params.ConnectionID)
+	suite.Equal(TransferChannel, params.TransferChannel)
+	suite.Equal(TransferPort, params.TransferPort)
+	suite.Equal(BaseDenom, params.BaseDenom)
+	suite.Equal(MintDenom, params.MintDenom)
+	suite.Equal(PstakeFeeAddress, params.PstakeFeeAddress)
+	suite.Equal(sdk.NewInt(5), params.MinDeposit)
+	suite.Equal(depositFee, params.PstakeDepositFee)
+	suite.Equal(restakeFee, params.PstakeRestakeFee)
+	suite.Equal(unstakeFee, params.PstakeUnstakeFee)
+}
