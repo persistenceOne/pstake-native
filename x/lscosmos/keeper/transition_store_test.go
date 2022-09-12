911ddaf9eaad953f81782bee6931116e6ce43219
arhamchordia <chordia.1@iitj.ac.in>
2022-09-12 18:21:23 +0400

    Updated transition_store_test.go

--- x/lscosmos/keeper/transition_store_test.go
+++ x/lscosmos/keeper/transition_store_test.go
@@ -10,12 +10,31 @@ func (suite *IntegrationTestSuite) TestTransitionStore() {
 
 	lscosmosKeeper := app.LSCosmosKeeper
 
-	ibcAmountTransitionStore := types.IbcAmountTransitionStore{
-		IbcTransfer: sdk.NewCoins(sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000))),
-		IcaDelegate: sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)),
+	// set, get and match IBCAmountTransientStore
+	ibcAmountTransitionStore := types.IBCAmountTransientStore{
+		IBCTransfer: sdk.NewCoins(sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000))),
+		ICADelegate: sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)),
 	}
-	lscosmosKeeper.SetIBCTransitionStore(ctx, ibcAmountTransitionStore)
+	lscosmosKeeper.SetIBCTransientStore(ctx, ibcAmountTransitionStore)
+	suite.Equal(ibcAmountTransitionStore, lscosmosKeeper.GetIBCTransientStore(ctx))
 
-	newIbcAmountTransitionStore := lscosmosKeeper.GetIBCTransitionStore(ctx)
-	suite.Equal(ibcAmountTransitionStore, newIbcAmountTransitionStore)
+	// add to IBC transfer in transient store
+	lscosmosKeeper.AddIBCTransferToTransientStore(ctx, sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	ibcAmountTransitionStore.IBCTransfer = ibcAmountTransitionStore.IBCTransfer.Add(sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	suite.Equal(ibcAmountTransitionStore, lscosmosKeeper.GetIBCTransientStore(ctx))
+
+	// remove from IBC transfer in transient store
+	lscosmosKeeper.RemoveIBCTransferFromTransientStore(ctx, sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	ibcAmountTransitionStore.IBCTransfer = sdk.NewCoins(sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	suite.Equal(ibcAmountTransitionStore, lscosmosKeeper.GetIBCTransientStore(ctx))
+
+	// add to ICA delegate in transient store
+	lscosmosKeeper.AddICADelegateToTransientStore(ctx, sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	ibcAmountTransitionStore.ICADelegate = ibcAmountTransitionStore.ICADelegate.Add(sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	suite.Equal(ibcAmountTransitionStore, lscosmosKeeper.GetIBCTransientStore(ctx))
+
+	// add to ICA delegate in transient store
+	lscosmosKeeper.RemoveICADelegateFromTransientStore(ctx, sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000)))
+	ibcAmountTransitionStore.ICADelegate = sdk.NewCoin(lscosmosKeeper.GetHostChainParams(ctx).BaseDenom, sdk.NewInt(10000))
+	suite.Equal(ibcAmountTransitionStore, lscosmosKeeper.GetIBCTransientStore(ctx))
 }
