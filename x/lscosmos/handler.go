331bd96ea3249effbb6a7487a7ae1f03c22b5e6c
arhamchordia <chordia.1@iitj.ac.in>
2022-09-20 13:11:41 +0400

    Adding message redeem to handler.go.

--- x/lscosmos/handler.go
+++ x/lscosmos/handler.go
@@ -29,6 +29,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgLiquidUnstake:
 			res, err := msgServer.LiquidUnstake(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgRedeem:
+			res, err := msgServer.Redeem(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
