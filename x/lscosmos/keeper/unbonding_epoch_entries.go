16c25afee1740cc377411bba9fa88517e1d7ea6c
puneetmahajan <puneet@persistence.one>
2022-09-29 23:07:26 +0400

    fix unbonding entry to append to existing one.

--- x/lscosmos/keeper/unbonding_epoch_entries.go
+++ x/lscosmos/keeper/unbonding_epoch_entries.go
@@ -20,8 +20,22 @@ func (k Keeper) SetDelegatorUnbondingEpochEntry(ctx sdk.Context, unbondingEpochE
 func (k Keeper) GetDelegatorUnbondingEpochEntry(ctx sdk.Context, delegatorAddress sdk.AccAddress, epochNumber int64) types.DelegatorUnbondingEpochEntry {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.GetDelegatorUnbondingEpochEntryKey(delegatorAddress, epochNumber))
+	if bz == nil {
+		return types.DelegatorUnbondingEpochEntry{}
+	}
 	var unbondingEpochEntry types.DelegatorUnbondingEpochEntry
 	k.cdc.MustUnmarshal(bz, &unbondingEpochEntry)
 
 	return unbondingEpochEntry
+}
+
+// AddDelegatorUnbondingEpochEntry adds delegator entry for unbondign stkatom for an unbonding epoch
+func (k Keeper) AddDelegatorUnbondingEpochEntry(ctx sdk.Context, delegatorAddress sdk.AccAddress, epochNumber int64, amount sdk.Coin) {
+	unbondingEntry := k.GetDelegatorUnbondingEpochEntry(ctx, delegatorAddress, epochNumber)
+	if unbondingEntry.Equal(types.DelegatorUnbondingEpochEntry{}) {
+		unbondingEntry = types.NewDelegatorUnbondingEpochEntry(delegatorAddress.String(), epochNumber, amount)
+	} else {
+		unbondingEntry.Amount = unbondingEntry.Amount.Add(amount)
+	}
+	k.SetDelegatorUnbondingEpochEntry(ctx, unbondingEntry)
 }
