ad02e1f3fff9f97c6122444712f0147455b1cd69
Puneet <59960662+puneet2019@users.noreply.github.com>
2022-12-05 15:34:47 +0400

    allow rest requests via grpc. (#383)

--- x/lscosmos/module.go
+++ x/lscosmos/module.go
@@ -1,6 +1,7 @@
 package lscosmos
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
@@ -73,6 +74,8 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	// this line is used by starport scaffolding # 2
+	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+
 }
 
 // GetTxCmd returns the capability module's root tx command.
