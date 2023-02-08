a22ec7e9f8bb81098aad291fb548959cefff94b9
Sunil Jalandhra <33174834+suniljalandhra@users.noreply.github.com>
2023-02-08 23:07:40 +0530

    chore : remove rest code

--- x/lscosmos/client/proposal_handler.go
+++ x/lscosmos/client/proposal_handler.go
@@ -1,28 +1,22 @@
 package client
 
 import (
-	"net/http"
-
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
-
 	"github.com/persistenceOne/pstake-native/x/lscosmos/client/cli"
 )
 
 var (
-	RegisterHostChainProposalHandler           = govclient.NewProposalHandler(cli.NewRegisterHostChainCmd, emptyRestHandler)
-	MinDepositAndFeeChangeProposalHandler      = govclient.NewProposalHandler(cli.NewMinDepositAndFeeChangeCmd, emptyRestHandler)
-	PstakeFeeAddressChangeProposalHandler      = govclient.NewProposalHandler(cli.NewPstakeFeeAddressChangeCmd, emptyRestHandler)
-	AllowListValidatorSetChangeProposalHandler = govclient.NewProposalHandler(cli.NewAllowListedValidatorSetChangeProposalCmd, emptyRestHandler)
+	RegisterHostChainProposalHandler           = govclient.NewProposalHandler(cli.NewRegisterHostChainCmd)
+	MinDepositAndFeeChangeProposalHandler      = govclient.NewProposalHandler(cli.NewMinDepositAndFeeChangeCmd)
+	PstakeFeeAddressChangeProposalHandler      = govclient.NewProposalHandler(cli.NewPstakeFeeAddressChangeCmd)
+	AllowListValidatorSetChangeProposalHandler = govclient.NewProposalHandler(cli.NewAllowListedValidatorSetChangeProposalCmd)
 )
 
-func emptyRestHandler(client.Context) govrest.ProposalRESTHandler {
-	return govrest.ProposalRESTHandler{
-		SubRoute: "unsupported-lscsmos-client",
-		Handler: func(w http.ResponseWriter, r *http.Request) {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Legacy REST Routes are not supported for lscosmos proposals")
-		},
-	}
-}
+//func emptyRestHandler(client.Context) govrest.ProposalRESTHandler {
+//	return govrest.ProposalRESTHandler{
+//		SubRoute: "unsupported-lscsmos-client",
+//		Handler: func(w http.ResponseWriter, r *http.Request) {
+//			rest.WriteErrorResponse(w, http.StatusBadRequest, "Legacy REST Routes are not supported for lscosmos proposals")
+//		},
+//	}
+//}
