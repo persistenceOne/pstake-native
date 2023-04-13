0a42733cbc0b95601138ec65e8006900322d71e6
kruspy <krusspy@gmail.com>
2023-04-13 18:25:58 +0200

    use buf for proto generation

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,29 +2,24 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
-  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
-    return 1
-  fi
+# get protoc executions
+go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos@latest 2>/dev/null
-}
-
-protoc_gen_gocosmos
-
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+echo "Generating gogo proto code"
+cd proto
+proto_dirs=$(find ./pstake -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf protoc \
-    -I "proto" \
-    -I "third_party/proto" \
-    --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-    --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep go_package "$file" &>/dev/null; then
+      buf generate --template buf.gen.gogo.yaml "$file"
+    fi
+  done
 done
+
+cd ..
 
 # move proto files to the right places
 cp -r github.com/persistenceOne/pstake-native/v2/* ./
 rm -rf github.com
+
+go mod tidy
