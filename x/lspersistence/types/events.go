035d10f1f38409be87b1cff437142f5239a4e302
puneetmahajan <puneet@persistence.one>
2023-05-16 12:14:13 +0530

    add fee to liquid stake.

--- x/lspersistence/types/events.go
+++ x/lspersistence/types/events.go
@@ -20,6 +20,10 @@ const (
 	AttributeKeyLiquidValidator       = "liquid_validator"
 	AttributeKeyRedelegationCount     = "redelegation_count"
 	AttributeKeyRedelegationFailCount = "redelegation_fail_count"
+	AttributeKeyPstakeDepositFee      = "pstake-deposit-fee"
+	AttributeKeyPstakeRestakeFee      = "pstake-restake-fee"
+	AttributeKeyPstakeRedeemFee       = "pstake-redeem-fee"
+	AttributeKeyPstakeUnstakeFee      = "pstake-unstake-fee"
 
 	AttributeKeyAuthority     = "authority"
 	AttributeKeyUpdatedParams = "updated_params"
