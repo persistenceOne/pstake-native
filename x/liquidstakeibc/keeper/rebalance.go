6d9c10a8c4f3573e48332d47f014b3d58caac710
puneetmahajan <puneet@persistence.one>
2023-11-24 19:07:15 +0530

    refactor rebalance so it is easier to test.

--- x/liquidstakeibc/keeper/rebalance.go
+++ x/liquidstakeibc/keeper/rebalance.go
@@ -19,6 +19,7 @@ type delegation struct {
 	validatorDetails types.Validator
 }
 
+// Rebalance tries to make redelegate transactions to host-chain to balance the delegations as per the weights.
 func (k Keeper) Rebalance(ctx sdk.Context, epoch int64) []proto.Message {
 
 	hcs := k.GetAllHostChains(ctx)
@@ -29,84 +30,8 @@ func (k Keeper) Rebalance(ctx sdk.Context, epoch int64) []proto.Message {
 			k.Logger(ctx).Info("redelegation epoch co-incides with unbonding epoch, skipping it")
 			continue
 		}
-		var AcceptableDelta = hc.Params.RedelegationAcceptableDelta
-		var MaxRedelegationEntries = hc.Params.MaxEntries
-		sum := math.ZeroInt()
-		for _, validator := range hc.Validators {
-			sum = sum.Add(validator.DelegatedAmount)
-		}
-
-		var idealDelegationList []delegation
-		sum2 := math.ZeroInt()
-		for i, validator := range hc.Validators {
-			idealAmt := validator.Weight.MulInt(sum).TruncateInt()
-			// last element
-			if i == len(hc.Validators)-1 {
-				idealAmt = sum.Sub(sum2)
-			}
-			sum2 = sum2.Add(idealAmt)
-			idealDelegationList = append(idealDelegationList,
-				delegation{
-					validator:        validator.OperatorAddress,
-					ideal:            idealAmt,
-					delegation:       validator.DelegatedAmount,
-					diff:             validator.DelegatedAmount.Sub(idealAmt),
-					validatorDetails: *validator,
-				})
-
-		}
-		// negative diffs first, so descending
-		idealDelegationList = sortDelegationListAsc(idealDelegationList)
-		revIdealList := idealDelegationList
-		// positive diffs first (descending)
-		Reverse(revIdealList)
-		redelegations, ok := k.GetRedelegations(ctx, hc.ChainId)
-		if !ok {
-			redelegations = &types.Redelegations{
-				ChainID:       hc.ChainId,
-				Redelegations: []*stakingtypes.Redelegation{},
-			}
-		}
-
-		var msgs []proto.Message
-	L1:
-		for i := range revIdealList {
-			if revIdealList[i].diff.LT(AcceptableDelta) {
-				break L1
-			}
-			if !k.RedelegationExistsToValidator(redelegations.Redelegations, revIdealList[i].validator) {
-				//re-sort idealDelegationAsc
-				idealDelegationList = sortDelegationListAsc(idealDelegationList)
-			L2:
-				for j := range idealDelegationList {
-					if revIdealList[i].validator == idealDelegationList[j].validator {
-						break L1
-					}
-					if !idealDelegationList[j].validatorDetails.Delegable {
-						continue L2
-					}
-					if idealDelegationList[j].diff.IsPositive() {
-						break L2
-					}
-					_, numEntries := k.RedelegationFromAToB(redelegations.Redelegations, revIdealList[i].validator, idealDelegationList[j].validator)
-					if numEntries < MaxRedelegationEntries {
-						redelegationAmt := math.MinInt(revIdealList[i].diff.Abs(), idealDelegationList[j].diff.Abs())
-						redelegateMsg := &stakingtypes.MsgBeginRedelegate{
-							DelegatorAddress:    hc.DelegationAccount.Address,
-							ValidatorSrcAddress: revIdealList[i].validator,
-							ValidatorDstAddress: idealDelegationList[j].validator,
-							Amount:              sdk.NewCoin(hc.HostDenom, redelegationAmt),
-						}
-						msgs = append(msgs, redelegateMsg)
-						revIdealList[i].diff = revIdealList[i].diff.Sub(redelegationAmt)
-						idealDelegationList[j].diff = idealDelegationList[j].diff.Add(redelegationAmt)
-						if revIdealList[i].diff.LT(AcceptableDelta) {
-							break L2
-						}
-					}
-				}
-			}
-		}
+		msgs := k.GenerateRedelegateMsgs(ctx, *hc)
+		// send one msg per ica
 		for _, msg := range msgs {
 			ibcSeq, err := k.GenerateAndExecuteICATx(ctx, hc.ConnectionId, hc.DelegationAccount.Owner, []proto.Message{msg})
 			if err != nil {
@@ -120,6 +45,89 @@ func (k Keeper) Rebalance(ctx sdk.Context, epoch int64) []proto.Message {
 		}
 	}
 	return nil
+}
+
+func (k Keeper) GenerateRedelegateMsgs(ctx sdk.Context, hc types.HostChain) []proto.Message {
+	var AcceptableDelta = hc.Params.RedelegationAcceptableDelta
+	var MaxRedelegationEntries = hc.Params.MaxEntries
+	sum := math.ZeroInt()
+	for _, validator := range hc.Validators {
+		sum = sum.Add(validator.DelegatedAmount)
+	}
+
+	var idealDelegationList []delegation
+	sum2 := math.ZeroInt()
+	for i, validator := range hc.Validators {
+		idealAmt := validator.Weight.MulInt(sum).TruncateInt()
+		// last element
+		if i == len(hc.Validators)-1 {
+			idealAmt = sum.Sub(sum2)
+		}
+		sum2 = sum2.Add(idealAmt)
+		idealDelegationList = append(idealDelegationList,
+			delegation{
+				validator:        validator.OperatorAddress,
+				ideal:            idealAmt,
+				delegation:       validator.DelegatedAmount,
+				diff:             validator.DelegatedAmount.Sub(idealAmt),
+				validatorDetails: *validator,
+			})
+
+	}
+	// negative diffs first, so ascending
+	idealDelegationList = sortDelegationListAsc(idealDelegationList)
+	revIdealList := make([]delegation, len(idealDelegationList))
+	copy(revIdealList, idealDelegationList)
+	// positive diffs first (descending)
+	Reverse(revIdealList)
+	redelegations, ok := k.GetRedelegations(ctx, hc.ChainId)
+	if !ok {
+		redelegations = &types.Redelegations{
+			ChainID:       hc.ChainId,
+			Redelegations: []*stakingtypes.Redelegation{},
+		}
+	}
+
+	var msgs []proto.Message
+L1:
+	for i := range revIdealList {
+		if revIdealList[i].diff.LT(AcceptableDelta) {
+			break L1
+		}
+		// RedelegationExistsToValidator: This is not updated inside the loop (with newer msgs), so some ICA redelegate txns might fail, and it is ok.
+		if !k.RedelegationExistsToValidator(redelegations.Redelegations, revIdealList[i].validator) {
+			//re-sort idealDelegationAsc
+			idealDelegationList = sortDelegationListAsc(idealDelegationList)
+		L2:
+			for j := range idealDelegationList {
+				if revIdealList[i].validator == idealDelegationList[j].validator {
+					break L1
+				}
+				if revIdealList[i].diff.LT(AcceptableDelta) || idealDelegationList[j].diff.IsPositive() {
+					break L2
+				}
+				if !idealDelegationList[j].validatorDetails.Delegable || idealDelegationList[j].validatorDetails.Status != stakingtypes.Bonded.String() {
+					continue L2
+				}
+
+				// RedelegationFromAToB: This is not updated inside the loop (with newer msgs), so some ICA redelegate txns might fail, and it is ok.
+				_, numEntries := k.RedelegationFromAToB(redelegations.Redelegations, revIdealList[i].validator, idealDelegationList[j].validator)
+				if numEntries < MaxRedelegationEntries {
+					redelegationAmt := math.MinInt(revIdealList[i].diff.Abs(), idealDelegationList[j].diff.Abs())
+					redelegateMsg := &stakingtypes.MsgBeginRedelegate{
+						DelegatorAddress:    hc.DelegationAccount.Address,
+						ValidatorSrcAddress: revIdealList[i].validator,
+						ValidatorDstAddress: idealDelegationList[j].validator,
+						Amount:              sdk.NewCoin(hc.HostDenom, redelegationAmt),
+					}
+					msgs = append(msgs, redelegateMsg)
+					revIdealList[i].diff = revIdealList[i].diff.Sub(redelegationAmt)
+					idealDelegationList[j].diff = idealDelegationList[j].diff.Add(redelegationAmt)
+				}
+			}
+		}
+	}
+	return msgs
 }
 
 func (k Keeper) RedelegationExistsToValidator(redelegations []*stakingtypes.Redelegation, toValoper string) bool {
