d7b62b96d38a728d7585c1e1c49070acccbea1bd
Puneet <59960662+puneet2019@users.noreply.github.com>
2024-01-12 01:33:35 +0530

    fix: fix hooks and add tests (#719)
    * fix hooks and add tests
    
    * lint
    
    * refactor ica_tx.go.
    
    * add tests to ibc, ica_tx..
    
    * more tests for msg_server_test.go
    
    * more tests for msg_server_test.go
    
    * fix changelog
    
    * more tests.

---
+++ x/ratesync/keeper/ica_tx_test.go
@@ -0,0 +1,15 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (suite *IntegrationTestSuite) TestExecuteLiquidStakeRateTx() {
+	k, ctx := suite.app.RatesyncKeeper, suite.ctx
+	_ = createNChain(k, ctx, 2)
+	hc, _ := k.GetHostChain(ctx, 1)
+	suite.Require().NoError(k.ExecuteLiquidStakeRateTx(ctx, hc.Features.LiquidStakeIBC,
+		"stk/uatom", "uatom", sdk.OneDec(), hc.ID, suite.ratesyncPathAB.EndpointA.ConnectionID, hc.ICAAccount))
+	suite.Require().NoError(k.InstantiateLiquidStakeContract(ctx, hc.ICAAccount,
+		hc.Features.LiquidStake, hc.ID, suite.ratesyncPathAB.EndpointA.ConnectionID))
+}
