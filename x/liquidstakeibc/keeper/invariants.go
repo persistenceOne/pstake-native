16ad451343fba949f2cd5bc61b629f3519747c51
kruspy <krusspy@gmail.com>
2024-01-23 13:09:44 +0100

    invariant fix

--- x/liquidstakeibc/keeper/invariants.go
+++ x/liquidstakeibc/keeper/invariants.go
@@ -16,19 +16,23 @@ func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
 func CValueLimits(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
 		hostChains := k.GetAllHostChains(ctx)
-		str := ""
+		strs := make([]string, 0)
 		broken := false
 		for _, hc := range hostChains {
 			if !k.CValueWithinLimits(hc) {
-				str = fmt.Sprintf("chainID: %s, cValue: %s \n", hc.ChainId, hc.CValue)
+				strs = append(strs, fmt.Sprintf("chainID: %s, cValue: %s \n", hc.ChainId, hc.CValue))
 			}
 		}
-		if str != "" {
+		invariantStr := ""
+		if len(strs) != 0 {
 			broken = true
+			for _, str := range strs {
+				invariantStr += fmt.Sprintf("%s\n", str)
+			}
 		}
 		return sdk.FormatInvariant(
 			types.ModuleName, "cvalue-limits",
-			fmt.Sprintf("cvalue out of bounds: %v, values as follows \n %s ", broken, str),
+			fmt.Sprintf("cvalue out of bounds: %v, values as follows \n %s ", broken, invariantStr),
 		), broken
 	}
 }
