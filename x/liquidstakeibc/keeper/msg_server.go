5144a90e9719b8c32c6975f0090532959cac34e1
kruspy <krusspy@gmail.com>
2024-01-25 14:49:45 +0100

    fix

--- x/liquidstakeibc/keeper/msg_server.go
+++ x/liquidstakeibc/keeper/msg_server.go
@@ -495,7 +495,8 @@ func (k msgServer) LiquidStakeLSM(
 		}
 
 		// check for minimum deposit amount
-		if delegation.Amount.LT(hc.MinimumDeposit) {
+		if sdktypes.NewDecFromInt(delegation.Amount).Mul(validator.ExchangeRate).
+			LT(sdktypes.NewDecFromInt(hc.MinimumDeposit)) {
 			return nil, errorsmod.Wrapf(
 				types.ErrMinDeposit,
 				"expected amount for delegation %s more than %s, got %s",
@@ -593,7 +594,7 @@ func (k msgServer) LiquidStakeLSM(
 				sdktypes.NewAttribute(types.AttributeChainID, hc.ChainId),
 				sdktypes.NewAttribute(types.AttributeDelegatorAddress, delegator.String()),
 				sdktypes.NewAttribute(types.AttributeInputAmount,
-					sdktypes.NewCoin(hc.HostDenom, delegation.Amount).String()),
+					sdktypes.NewCoin(hc.HostDenom, deposit.Amount).String()),
 				sdktypes.NewAttribute(types.AttributeOutputAmount,
 					sdktypes.NewCoin(hc.MintDenom(), mintToken.Sub(protocolFee).Amount).String()),
 				sdktypes.NewAttribute(types.AttributePstakeDepositFee,
