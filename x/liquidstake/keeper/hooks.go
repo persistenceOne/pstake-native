4089d0a5231a5fba5a9f896677566b5815872ad7
kruspy <krusspy@gmail.com>
2024-03-12 12:24:24 +0400

    add pause flag to the hook

--- x/liquidstake/keeper/hooks.go
+++ x/liquidstake/keeper/hooks.go
@@ -27,9 +27,12 @@ func (h EpochHooks) AfterEpochEnd(_ sdk.Context, _ string, _ int64) error {
 }
 
 func (k Keeper) BeforeEpochStart(ctx sdk.Context, epochIdentifier string, epochNumber int64) error {
-	// Update the liquid validator set at the start of each epoch
-	if epochIdentifier == liquidstake.AutocompoundEpoch {
-		k.AutocompoundStakingRewards(ctx, liquidstake.GetWhitelistedValsMap(k.GetParams(ctx).WhitelistedValidators))
+
+	if !k.GetParams(ctx).ModulePaused {
+		// Update the liquid validator set at the start of each epoch
+		if epochIdentifier == liquidstake.AutocompoundEpoch {
+			k.AutocompoundStakingRewards(ctx, liquidstake.GetWhitelistedValsMap(k.GetParams(ctx).WhitelistedValidators))
+		}
 	}
 
 	return nil
