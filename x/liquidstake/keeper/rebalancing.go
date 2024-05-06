584bd3d8ee6006f505ac968769d5416d5a83b537
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-05-06 14:07:00 +0330

    Fix a possible bug

--- x/liquidstake/keeper/rebalancing.go
+++ x/liquidstake/keeper/rebalancing.go
@@ -262,7 +262,11 @@ func (k Keeper) AutocompoundStakingRewards(ctx sdk.Context, whitelistedValsMap t
 
 	// subtract fee from accumulated rewards
 	acRewards := k.GetAccumulatingRewards(ctx)
-	acRewards = acRewards.Sub(autocompoundFee.Amount)
+	if autocompoundFee.Amount.GTE(acRewards) {
+		acRewards = sdk.ZeroInt()
+	} else {
+		acRewards = acRewards.Sub(autocompoundFee.Amount)
+	}
 	k.SetAccumulatingRewards(ctx, acRewards)
 
 	ctx.EventManager().EmitEvents(sdk.Events{
