01f68cc9408068736c224fd1dc69e4da315872a4
puneetmahajan <puneetmahajan1996@gmail.com>
2024-12-13 17:11:19 +0400

    remove redeem deprecated

--- x/liquidstakeibc/types/msgs.go
+++ x/liquidstakeibc/types/msgs.go
@@ -24,7 +24,6 @@ const (
 	MsgTypeLiquidUnstake     string = "msg_liquid_unstake"
 	MsgTypeRedeem            string = "msg_redeem"
 	MsgTypeUpdateParams      string = "msg_update_params"
-	MsgTypeRedeemDeprecated  string = "msg_redeem_deprecated"
 )
 
 var (
@@ -34,7 +33,7 @@ var (
 	_ sdk.Msg = &MsgLiquidUnstake{}
 	_ sdk.Msg = &MsgRedeem{}
 	_ sdk.Msg = &MsgLiquidStakeLSM{}
-	_ sdk.Msg = &MsgRedeemDeprecated{}
+	_ sdk.Msg = &MsgUpdateParams{}
 )
 
 func NewMsgRegisterHostChain(
@@ -675,56 +674,6 @@ func (m *MsgUpdateParams) ValidateBasic() error {
 	err := m.Params.Validate()
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-func NewMsgRedeemDeprecated(amount sdk.Coin, address sdk.AccAddress) *MsgRedeemDeprecated {
-	return &MsgRedeemDeprecated{
-		DelegatorAddress: address.String(),
-		Amount:           amount,
-	}
-}
-
-func (m *MsgRedeemDeprecated) Route() string {
-	return RouterKey
-}
-
-// Type should return the action
-func (m *MsgRedeemDeprecated) Type() string {
-	return MsgTypeRedeemDeprecated
-}
-
-// GetSignBytes encodes the message for signing
-func (m *MsgRedeemDeprecated) GetSignBytes() []byte {
-	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
-}
-
-// GetSigners defines whose signature is required
-func (m *MsgRedeemDeprecated) GetSigners() []sdk.AccAddress {
-	acc, err := sdk.AccAddressFromBech32(m.DelegatorAddress)
-	if err != nil {
-		panic(err)
-	}
-	return []sdk.AccAddress{acc}
-}
-
-// ValidateBasic performs stateless checks
-func (m *MsgRedeemDeprecated) ValidateBasic() error {
-	if _, err := sdk.AccAddressFromBech32(m.DelegatorAddress); err != nil {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, m.DelegatorAddress)
-	}
-
-	if !m.Amount.IsValid() {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidCoins, m.Amount.String())
-	}
-
-	if !m.Amount.IsPositive() {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidCoins, m.Amount.String())
-	}
-
-	if !IsLiquidStakingDenom(m.Amount.Denom) {
-		return sdkerrors.ErrInvalidCoins.Wrapf("invalid denom, required stk/{host-denom} got %s", m.Amount.Denom)
 	}
 	return nil
 }
--- x/liquidstakeibc/types/msgs.pb.gw.go
+++ x/liquidstakeibc/types/msgs.pb.gw.go
@@ -177,42 +177,6 @@ func local_request_Msg_Redeem_0(ctx context.Context, marshaler runtime.Marshaler
 
 }
 
-var (
-	filter_Msg_RedeemDeprecated_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
-func request_Msg_RedeemDeprecated_0(ctx context.Context, marshaler runtime.Marshaler, client MsgClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MsgRedeemDeprecated
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_RedeemDeprecated_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.RedeemDeprecated(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_Msg_RedeemDeprecated_0(ctx context.Context, marshaler runtime.Marshaler, server MsgServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq MsgRedeemDeprecated
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Msg_RedeemDeprecated_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.RedeemDeprecated(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 // RegisterMsgHandlerServer registers the http handlers for service Msg to "mux".
 // UnaryRPC     :call MsgServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -308,29 +272,6 @@ func RegisterMsgHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 		}
 
 		forward_Msg_Redeem_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("POST", pattern_Msg_RedeemDeprecated_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_Msg_RedeemDeprecated_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Msg_RedeemDeprecated_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -455,26 +396,6 @@ func RegisterMsgHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 
 	})
 
-	mux.Handle("POST", pattern_Msg_RedeemDeprecated_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_Msg_RedeemDeprecated_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Msg_RedeemDeprecated_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
@@ -486,8 +407,6 @@ var (
 	pattern_Msg_LiquidUnstake_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"pstake", "liquidstakeibc", "v1beta1", "LiquidUnstake"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Msg_Redeem_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"pstake", "liquidstakeibc", "v1beta1", "Redeem"}, "", runtime.AssumeColonVerbOpt(false)))
-
-	pattern_Msg_RedeemDeprecated_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"pstake", "liquidstakeibc", "v1beta1", "RedeemDeprecated"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
@@ -498,6 +417,4 @@ var (
 	forward_Msg_LiquidUnstake_0 = runtime.ForwardResponseMessage
 
 	forward_Msg_Redeem_0 = runtime.ForwardResponseMessage
-
-	forward_Msg_RedeemDeprecated_0 = runtime.ForwardResponseMessage
 )
