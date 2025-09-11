0366280fa75f482acc2f259b69a0ca467e5ca194
puneetmahajan <puneetmahajan1996@gmail.com>
2025-09-11 19:50:48 +0400

    fix testnet cmd

--- cmd/pstaked/cmd/testnet.go
+++ cmd/pstaked/cmd/testnet.go
@@ -228,6 +228,7 @@ func InitTestnet(
 			sdk.NewCoin(sdk.DefaultBondDenom, valTokens),
 			stakingtypes.NewDescription(nodeDirName, "", "", "", ""),
 			stakingtypes.NewCommissionRates(math.LegacyOneDec(), math.LegacyOneDec(), math.LegacyOneDec()),
+			math.OneInt(),
 		)
 		if err != nil {
 			return err
